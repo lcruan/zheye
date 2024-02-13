39b05edb60b670e8e08ce5eabb7b828519c88ba7
lcruan <1416802331@qq.com>
2024-02-13 16:27:12 +0800

    dropdown 组件点击外边隐藏菜单

--- README.md
+++ README.md
@@ -1,24 +1,12 @@
-# zheye
+### 如何通过 ref 来获取 dom对象？
 
-## Project setup
-```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
+1. vue3提供一个简单的方式，在setup中提供一个ref对象来获取dom节点
+- 注意：要做到返回的名称与结构中定义的ref命名完全一样，当dom真正挂载的时候，我们就可以从dropdownRef.value拿到节点
+- ref的类型：在setup中还不是dom节点类型，挂载后则是dom节点，所以初始类型则是两者的联合类型
+```html
+<div class="dropdown" ref="dropdownRef">
 ```
 
-### Compiles and minifies for production
+```js
+const dropdownRef = ref<null | HTMLElement>(null) 
 ```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="dropdown">
+  <div class="dropdown" ref="dropdownRef">
     <a href="#" class="btn btn-outline-light my-2 dropdown-toggle" @click.prevent="toggleOpen">{{
       title
     }}</a>
@@ -10,7 +10,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, onMounted, onUnmounted } from 'vue'
 
 export default defineComponent({
   name: 'Dropdown',
@@ -22,12 +22,31 @@ export default defineComponent({
   },
   setup () {
     const isOpen = ref(false)
+    // vue3 中提供新建一个ref对象来获取dom节点 *注意记得将ref返回出去，否则不生效
+    // 在setup中还不是dom节点类型，挂载后则是dom节点，所以初始类型则是两者的联合类型
+    const dropdownRef = ref<null | HTMLElement>(null) // 类似于 vue2中 结构中定义的 ref来获取 dom对象
     const toggleOpen = () => {
       isOpen.value = !isOpen.value
     }
+    const handler = (e: MouseEvent) => {
+      debugger
+      if (dropdownRef.value) {
+        // contains判断是否包含另一个节点
+        if (!dropdownRef.value.contains(e.target as HTMLElement) && isOpen.value) {
+          isOpen.value = false
+        }
+      }
+    }
+    onMounted(() => {
+      document.addEventListener('click', handler)
+    })
+    onUnmounted(() => {
+      document.removeEventListener('click', handler)
+    })
     return {
       isOpen,
-      toggleOpen
+      toggleOpen,
+      dropdownRef
     }
   }
 })
