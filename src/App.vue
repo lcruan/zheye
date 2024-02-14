4204f0930f81f042ae54f56be73b8e4b29faff19
lcruan <1416802331@qq.com>
2024-02-14 17:01:35 +0800

    抽象验证规则

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,49 @@
 <template>
   <div class="container">
     <global-header :user="currentUser"></global-header>
-    <column-list :list="list"></column-list>
+    <!-- <column-list :list="list"></column-list> -->
+    <form>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">邮箱地址</label>
+        <validate-input :rules="emailRules"></validate-input>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">邮箱地址</label>
+        <input
+          type="email"
+          class="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+          v-model="emailRef.val"
+          @blur="validateEmail"
+        />
+        <div class="form-text" v-if="emailRef.error">{{ emailRef.message }}</div>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">密码</label>
+        <input
+          type="password"
+          class="form-control"
+          id="exampleInputPassword1"
+        />
+      </div>
+    </form>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, reactive } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+// import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+import ValidateInput, { RulesProp } from './components/ValidateInput.vue'
+import { ColumnProps } from './components/ColumnList.vue'
 import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
 const currentUser: UserProps = {
   isLogin: true,
   name: 'viking'
 }
+// 邮箱校验正则表达式
+const emailReg = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 const testData: ColumnProps[] = [
   {
     id: 1,
@@ -25,31 +55,65 @@ const testData: ColumnProps[] = [
     id: 2,
     title: 'test2的专栏',
     description: '这是test2专栏，有一段非常有意思的简介，可以更新下',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5f3e3a17c305b1070f455202.jpg?x-oss-process=image/resize,m_pad,h_50,w_50'
+    avatar:
+      'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5f3e3a17c305b1070f455202.jpg?x-oss-process=image/resize,m_pad,h_50,w_50'
   },
   {
     id: 3,
     title: 'test3的专栏',
     description: '这是test3专栏，有一段非常有意思的简介，可以更新下',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5f3e3a17c305b1070f455202.jpg?x-oss-process=image/resize,m_pad,h_50,w_50'
+    avatar:
+      'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5f3e3a17c305b1070f455202.jpg?x-oss-process=image/resize,m_pad,h_50,w_50'
   },
   {
     id: 4,
     title: 'test4的专栏',
     description: '这是test24专栏，有一段非常有意思的简介，可以更新下',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5f3e3a17c305b1070f455202.jpg?x-oss-process=image/resize,m_pad,h_50,w_50'
+    avatar:
+      'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5f3e3a17c305b1070f455202.jpg?x-oss-process=image/resize,m_pad,h_50,w_50'
   }
 ]
 export default defineComponent({
   name: 'App',
   components: {
-    ColumnList,
-    GlobalHeader
+    // ColumnList,
+    GlobalHeader,
+    ValidateInput
   },
   setup () {
+    const emailRules: RulesProp = [
+      {
+        type: 'required',
+        message: '电子邮箱地址不能为空'
+      },
+      {
+        type: 'email',
+        message: '请输入正确的电子邮箱格式'
+      }
+    ]
+    // 定义email校验对象格式
+    const emailRef = reactive({
+      val: '',
+      error: false,
+      message: ''
+    })
+    // 定义email校验函数
+    const validateEmail = () => {
+      if (emailRef.val.trim() === '') {
+        emailRef.error = true
+        emailRef.message = 'can not be empty'
+      } else if (!emailReg.test(emailRef.val)) {
+        emailRef.error = true
+        emailRef.message = 'should be valid email'
+      }
+    }
+
     return {
       list: testData,
-      currentUser
+      currentUser,
+      emailRef,
+      validateEmail,
+      emailRules
     }
   }
 })
